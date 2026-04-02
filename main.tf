1c395a64d6a5e9cc7f5d50f8c89b7721acdfc2ce
Sharath Koppula <koppulasharath@gmail.com>
2026-04-02 13:14:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,44 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
-resource "aws_instance" "devops_server" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Amazon Linux (Mumbai)
+# Security Group
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "devops_instance" {
+  ami           = var.ami_id
   instance_type = var.instance_type
 
+  security_groups = [aws_security_group.devops_sg.name]
+
   tags = {
-    Name = "Terraform-EC2"
+    Name = "DevOps-Instance"
   }
 }
