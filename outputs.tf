2dd485613dcaf5c2df22e606b14f4827acf1f44e
Sharath Koppula <koppulasharath@gmail.com>
2026-04-02 13:18:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "instance_public_ip" {
-  description = "Public IP of EC2"
-  value       = aws_instance.devops_server.public_ip
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.devops_instance.public_ip
+}
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.devops_instance.id
 }
