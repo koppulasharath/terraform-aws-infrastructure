d6d3223696519c325fb2eed9ef1a8526c3281959
Sharath Koppula <koppulasharath@gmail.com>
2026-04-02 13:16:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "eu-north-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  default     = "ami-xxxxxxxxxxxxx"  # replace with your AMI
+}
+
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "EC2 Instance Type"
   default     = "t2.micro"
 }
